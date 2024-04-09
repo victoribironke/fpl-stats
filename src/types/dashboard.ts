4e15b14801e11430afe83e92172c126bf2b499e5
Victor Ibironke <victoribironke2019@gmail.com>
2024-04-09 22:17:27 +0100

    fixed lint errors

--- src/types/dashboard.ts
+++ src/types/dashboard.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 export type DashboardTemplateProps = {
   children: React.ReactNode;
 };
--- src/types/general.ts
+++ src/types/general.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 export type ModalProps = {
   header: string;
   dismiss: () => void;
