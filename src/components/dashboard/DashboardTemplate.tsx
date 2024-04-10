72529990a8fcb2729af296ed964c5357e3af2c0c
Victor Ibironke <victoribironke2019@gmail.com>
2024-04-10 09:32:09 +0100

    fixed error with name

--- src/components/dashboard/DashboardTemplate.tsx
+++ src/components/dashboard/DashboardTemplate.tsx
@@ -3,14 +3,16 @@ import { user_details } from "@/atoms/atoms";
 import { useRecoilValue } from "recoil";
 
 const DashboardTemplate = ({ children }: DashboardTemplateProps) => {
-  const { name } = useRecoilValue(user_details) as User;
+  const user = useRecoilValue(user_details);
 
   return (
     <>
       <section className="w-full min-h-screen flex items-center flex-col">
         <div className="w-full bg-white border-b p-4 flex items-center justify-center">
           <div className="w-full max-w-[1280px]">
-            <button className="bg-gray-100 py-1 px-3 rounded-md">{name}</button>
+            <button className="bg-gray-100 py-1 px-3 rounded-md">
+              {user?.name}
+            </button>
           </div>
         </div>
 
